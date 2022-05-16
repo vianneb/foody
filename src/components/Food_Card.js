a5544fbea8f52d9a12fc043e2ea5ab624906efa1
KaterynaTymofeieva <kattym@uw.edu>
2022-05-16 15:10:30 -0700

    props edit in food card

--- src/components/Food_Card.js
+++ src/components/Food_Card.js
@@ -6,7 +6,7 @@ export function FoodCard(props) {
       <div className="d-flex col-md-12 col-xl-6">
         <a className="card mb-4 w-100 bg-c-yellow stretched-link text-dec" href="#">
           <div className="card-body">
-            <h2 className="card-title text-center txt-c-white">Category</h2>
+            <h2 className="card-title text-center txt-c-white">{props.category}</h2>
           </div>
         </a>
       </div>
