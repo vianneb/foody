81d8e29cb664c11889cbca2372ddb85e7f560cf8
KaterynaTymofeieva <kattym@uw.edu>
2022-05-31 11:40:19 -0700

    add descriptions

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,10 @@ export function HomePage(props) {
 
   // define arrays for testing
   let foodList = [
-    { title: "American", img: "img/burger.jpg", description: "lorem ipsum" },
-    { title: "Italian", img: "img/pasta.jpg", description: "lorem ipsum" },
-    { title: "Mexican", img: "img/tacos.jpg", description: "lorem ipsum" },
-    { title: "Japanese", img: "img/sushi.jpg", description: "lorem ipsum" }];
+    { title: "American", img: "img/burger.jpg", description: "Explore American restaurants near Seattle on Foody: burgers, sandwiches, BBQ and more." },
+    { title: "Italian", img: "img/pasta.jpg", description: "Explore Italian restaurants near Seattle on Foody: pasta, antipasti, seafood, rissoto and more." },
+    { title: "Mexican", img: "img/tacos.jpg", description: "Explore Mexican restaurants near Seattle on Foody: tacos, burritos, fajitas and more." },
+    { title: "Japanese", img: "img/sushi.jpg", description: "Explore Japanese restaurants near Seattle on Foody: sushi, ramen, izakayas and more." }];
 
   let tagList = ["Vegan", "Gluten-free", "Dairy-free", "Nut-free", "Soy-free", "Seafood-free"];
 
