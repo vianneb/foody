2533d35b23cef78758bdb9791b10827003dec4ef
Vianne Buinguyen <vianneb@uw.edu>
2022-06-05 16:16:00 -0700

    implement filtering with cuisine cards on home page

--- src/components/Food_Card.js
+++ src/components/Food_Card.js
@@ -1,8 +1,22 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 
 
 export function FoodCard(props) {
+
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    const clickedCuisine = event.target.parentElement.querySelector('.card-title').textContent.toLowerCase();
+    const cuisineRestaurants = props.filteredRestaurants.filter((restaurant) => {
+      return restaurant.Cuisine.toLowerCase().includes(clickedCuisine);
+    })
+
+    props.setFilteredRestaurants(cuisineRestaurants);
+
+    navigate("/search");
+  }  
+  
   return (
     <div className="d-flex col-md-6 col-xl-3 " >
       <div className={"card mb-4 hover-c-yellow "}>
@@ -14,7 +28,7 @@ export function FoodCard(props) {
             <div className="col-sm">
               <h2 className="card-title">{props.foodObj.title}</h2>
               <p className="card-text">{props.foodObj.description}</p>
-              <button className="btn orange-btn btn-dark" href="#">Explore</button>
+              <button className="btn orange-btn btn-dark" href="#" onClick={handleClick}>Explore</button>
             </div>
           </div>
         </div>
@@ -26,7 +40,7 @@ export function FoodCard(props) {
 
 export function FoodCardList(props) {
   let foodItems = props.foodList.map((foodObj) => {
-    let component = <FoodCard key={foodObj.title} foodObj={foodObj} />
+    let component = <FoodCard key={foodObj.title} foodObj={foodObj} setFilteredRestaurants={props.setFilteredRestaurants} filteredRestaurants={props.filteredRestaurants}/>
     return component
   })
 
